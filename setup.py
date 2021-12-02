92889d44ea97b47a35f13146d914a310399bdc8b
TW Webb <Theodore.Webb@mvwc.com>
2021-12-02 14:41:12 -0500

    setup.py: defining packages.

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     description='Utility functions used across workstreams.',
     url='https://github.com/theodore-webb/py-mvw',
     author='TW Webb',
+    packages=['mvw'],
     entry_points = {
         'console_scripts': ['mvw-cmd=mvw.__init__:init'],
     }
