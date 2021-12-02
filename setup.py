39785cfca8590d38571c7d5eebc699b53eae2254
TW Webb <Theodore.Webb@mvwc.com>
2021-12-02 11:27:30 -0500

    setup.py: adding details of package.

--- setup.py
+++ setup.py
@@ -3,4 +3,8 @@ from setuptools import setup
 
 setup(
     name="mvw",
+    version='0.0.1',
+    description='Utility functions used across workstreams.',
+    url='https://github.com/theodore-webb/py-mvw',
+    author='TW Webb'
 )
